9145a57e51e6af30e8166952ad3e64184a2b6d78
houjinan <houjinan@126.com>
2015-09-06 13:16:39 +0800

    modified deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :format, :pretty
 set :log_level, :debug
 set :pty, true
 
-set :linked_files, %w{config/database.yml config/settings/production.yml}
+set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp public/system public/assets public/uploads public/soundink_codes}
 
 set :keep_releases, 5
