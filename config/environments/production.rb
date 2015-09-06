d5fe221891c357b8929196c11e6dc37196b94a58
houjinan <houjinan@126.com>
2015-09-06 17:03:36 +0800

    modified environment production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,17 +19,17 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
-  config.action_controller.relative_url_root = '/discount_service'
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.serve_static_files = false
+  config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
+  config.assets.css_compressor = :sass
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
