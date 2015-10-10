59c6dc63b2e99f0acfd2271b8b1c39f50fad6444
houjinan <houjinan@126.com>
2015-10-10 21:22:23 +0800

    add admin account

---
+++ app/controllers/account/dashboard_controller.rb
@@ -0,0 +1,6 @@
+module Account
+  class DashboardController < AccountController
+    def index
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+    resources :users
+  end
+
+  namespace :account do 
+    get 'dashboard' => 'dashboard#index', as: 'dashboard'
+    resources :articles
+  end
+
   resources :articles
   devise_for :users
 
-  
   get 'home/index'
   get 'home/feel'
   get 'home/about_me'
