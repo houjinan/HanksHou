07ab729a2cd6de40d8a12636adf1258556662e2f
houjinan <houjinan@126.com>
2015-10-10 11:13:34 +0800

    added home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
   def index
-    #binding.pry
+    cookies["nav_active"] = "index"
+  end
+
+  def feel
+    cookies["nav_active"] = "feel"
+  end
+
+  def about_me
+    render "about_me", layout: "about_me"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   
   get 'home/index'
+  get 'home/feel'
+  get 'home/about_me'
   root 'home#index'
 end
