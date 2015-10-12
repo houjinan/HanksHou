662a0c80d4a08ffe8a78d5b118a6e04d47892ebc
houjinan <houjinan@126.com>
2015-10-12 11:20:05 +0800

    fixed jump css not load

---
+++ app/controllers/account/account_controller.rb
@@ -0,0 +1,7 @@
+module Account
+  class AccountController < ApplicationController
+    before_action :authenticate_user!
+    layout 'account_layout'
+
+  end
+end
--- app/controllers/account_controller.rb
+++
@@ -1,6 +0,0 @@
-class AccountController < ApplicationController
-  before_action :authenticate_user!
-  layout 'account_layout'
-
-
-end
