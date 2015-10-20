84bd77e2363ea034098f4e585edf296acd7ab293
houjinan <houjinan@126.com>
2015-10-20 15:11:00 +0800

    add search

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,12 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show]
   def index
-    @articles = Article.desc("created_at").paginate(:per_page => 10, :page => params[:page])
+    @articles = Article.desc("created_at")
     if params[:label_id].present? 
       @articles = Label.find(params[:label_id]).articles
+    end
+    if params[:search].present?
+      @articles = @articles.any_of({:title => Regexp.new(".*"+params[:search]+".*")}, {:content => Regexp.new(".*"+params[:search]+".*") })
     end
     @articles = @articles.paginate(:per_page => 10, :page => params[:page])
     @labels = Label.all
