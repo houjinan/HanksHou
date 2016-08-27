6cd8d40da239bdbe665f322d87338278e90feacb
HanksHou <houjinan@126.com>
2016-08-27 14:28:56 +0800

    add auth to api swaggers

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -7,6 +7,14 @@ GrapeSwaggerRails.options.before_filter_proc = proc {
 GrapeSwaggerRails.options.app_name = 'HanksHou Apis'
 
 
+GrapeSwaggerRails.options.before_filter do |request|
+  GrapeSwaggerRails.options.app_url = request.protocol + request.host_with_port
+  if current_user.blank? || !current_user.is_super_admin?
+    request.session[:user_return_to] = request.url
+    redirect_to "/users/sign_in"
+  end 
+end
+
 # GrapeSwaggerRails.options.app_url  = 'http://localhost:3300'
 # GrapeSwaggerRails.options.api_auth     = 'basic'
 # GrapeSwaggerRails.options.api_key_name = 'Authorization'
