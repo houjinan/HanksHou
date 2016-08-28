72aa40ad1c742e86ab2d8d9d04ee7c48ece38a2e
HanksHou <houjinan@126.com>
2016-08-28 22:25:39 +0800

    fix not show head

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,8 +21,9 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-
-  config.public_file_server.enabled = false
+  
+  config.serve_static_files = true
+  # config.public_file_server.enabled = false
   # config.serve_static_files.enabled = false
 
   # Compress JavaScripts and CSS.
