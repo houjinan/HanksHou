172606984fe7c31a1573b8c7cf4037977bfcdaa0
houjinan@126.com <houjinan@126.com>
2016-09-08 09:28:47 +0800

    update env production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
@@ -21,8 +21,9 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  
-  config.serve_static_files = true
+
+  #config.serve_static_files = true
+  config.public_file_server.enabled = true
   # config.public_file_server.enabled = false
   # config.serve_static_files.enabled = false
 
