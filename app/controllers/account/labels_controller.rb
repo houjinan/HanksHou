8576f0ef0e6d883bd34a122c4df88483cdad33a7
HanksHou <houjinan@126.com>
2016-09-14 22:11:29 +0800

    lables update

--- app/controllers/account/labels_controller.rb
+++ app/controllers/account/labels_controller.rb
@@ -1,7 +1,20 @@
 module Account
   class LabelsController < Account::AccountController
     def index
+      @labels = Label.all.paginate(:per_page => 20, :page => params[:page])
+    end
 
+
+    def edit
+      @label = Label.find(params[:id])
+    end
+
+    def update
+
+    end
+
+    def destroy
+      @label = Label.find(params[:id])
     end
   end
 
