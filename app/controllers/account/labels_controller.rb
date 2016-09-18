1c1a97374affc0cfbae8c1e3a3a1109e855e12ce
houjinan@126.com <houjinan@126.com>
2016-09-18 16:51:16 +0800

    update label

--- app/controllers/account/labels_controller.rb
+++ app/controllers/account/labels_controller.rb
@@ -10,18 +10,30 @@ module Account
     end
 
     def update
-
+      @label = Label.find params[:id]
+      if @label.update(label_params)
+        redirect_to action: :index
+      else
+        render :edit
+      end
     end
 
     def destroy
       @label = Label.find(params[:id])
       if @label.articles.present?
-        flash[:error] = "exist label's articles" 
+        flash[:error] = "标签存在已经使用的文章不能删除！"
       else
+        flash[:notice] = "删除成功！"
         @label.destroy
       end
-      redirect_to :index
+      redirect_to action: :index
     end
+
+
+    private
+      def label_params
+        params.require(:label).permit(:name)
+      end
   end
 
 end
