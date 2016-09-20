0f469a230e44fa9c21c9ac71d7466a17dc8dd637
houjinan@126.com <houjinan@126.com>
2016-09-20 16:18:45 +0800

    update account scaffold

--- lib/generators/account_scaffold/account_scaffold_generator.rb
+++ lib/generators/account_scaffold/account_scaffold_generator.rb
@@ -1,22 +1,29 @@
 require 'rails/generators/resource_helpers'
-require 'rails/generators/mongoid_generator'
+require "rails/generators/model_helpers"
 
 module Rails
   module Generators
     class AccountScaffoldGenerator < Rails::Generators::NamedBase
 
-
-      include ResourceHelpers
+      include Rails::Generators::ModelHelpers
+      include Rails::Generators::ResourceHelpers
+      # remove_hook_for :resource_controller
+      # remove_class_option :actions
 
       check_class_collision suffix: "Controller"
 
-      argument :attributes, type: :array, default: [], banner: "field:type field:type"
+      argument :attributes, type: :array, default: [], banner: "field[:type][:index] field[:type][:index]"
 
       class_option :disable_common, type: :boolean, default: false, desc: 'SETS: --no-helper --no-view-specs --no-request-specs --no-routing-specs'
+
+      source_root File.expand_path('../templates', __FILE__)
 
       # def create_model_file
       #   template "model.rb.tt", File.join("app/models", class_path, "#{file_name}.rb")
       # end
+      hook_for :orm, :in => :rails, :required => true, :as => :model do |invoked|
+        invoke invoked, [singular_name]
+      end
 
       def create_controller_files
         if @options[:disable_common]
@@ -52,6 +59,7 @@ module Rails
       # hook_for :orm, as: :scaffold do |invoked|
       #   invoke invoked, [class_name]
       # end
+
     end
   end
 end
--- lib/templates/rails/account_scaffold/controller.rb
+++ lib/templates/rails/account_scaffold/controller.rb
@@ -1,57 +1,61 @@
-class <%=name.pluralize.camelize%>Controller < Account::AccountController
-  before_action :set_<%=singular_name%>, only: [:show, :edit, :update, :destroy]
+<% empty = "  " * class_path.size;class_path.each_with_index do |module_name, i|%><%= "  " * i%>module <%=module_name.camelize%>
+<% end %>
+<%= empty %>class <%=singular_name.camelize%>Controller < Account::AccountController
+<%= empty %>  before_action :set_<%=singular_name%>, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @<%=singular_name.pluralize %> = <%=singular_name.camelize %>.all.desc("created_at").paginate(:page => params[:page])
-  end
+<%= empty %>  def index
+<%= empty %>    @<%=singular_name.pluralize %> = <%=singular_name.camelize %>.all.desc("created_at").paginate(:page => params[:page])
+<%= empty %>  end
 
-  def show
+<%= empty %>  def show
 
-  end
+<%= empty %>  end
 
-  def new
-    @<%=singular_name %> = <%=singular_name.camelize %>.new()
-  end
+<%= empty %>  def new
+<%= empty %>    @<%=singular_name %> = <%=singular_name.camelize %>.new()
+<%= empty %>  end
 
-  def edit
+<%= empty %>  def edit
 
-  end
+<%= empty %>  end
 
-  def create
-    @<%=singular_name%> = <%=singular_name.camelize %>.new(<%=singular_name%>_params)
-    if @<%=singular_name%>.save
-      flash[:notice] = '创建成功'
-      redirect_to ({action: :show}.merge(id: @<%=singular_name%>.id))
-    else
-      render :new
-    end
-  end
+<%= empty %>  def create
+<%= empty %>    @<%=singular_name%> = <%=singular_name.camelize %>.new(<%=singular_name%>_params)
+<%= empty %>    if @<%=singular_name%>.save
+<%= empty %>      flash[:notice] = '创建成功'
+<%= empty %>      redirect_to ({action: :show}.merge(id: @<%=singular_name%>.id))
+<%= empty %>    else
+<%= empty %>      render :new
+<%= empty %>    end
+<%= empty %>  end
 
-  def update
-    if @<%=singular_name%>.update(<%=singular_name%>_params)
-      flash[:notice] = '成功更新'
-      redirect_to ({action: :show}.merge(id: @<%=singular_name%>.id))
-    else
-      render :edit
-    end
-  end
+<%= empty %>  def update
+<%= empty %>    if @<%=singular_name%>.update(<%=singular_name%>_params)
+<%= empty %>      flash[:notice] = '成功更新'
+<%= empty %>      redirect_to ({action: :show}.merge(id: @<%=singular_name%>.id))
+<%= empty %>    else
+<%= empty %>      render :edit
+<%= empty %>    end
+<%= empty %>  end
 
-  def destory
-    if @<%=singular_name%>.destroy
-      flash[:notice] = '成功删除'
-      redirect_to action: :index
-    else
-      flash[:error] = '删除失败'
-      redirect_to action: :index
-    end
-  end
+<%= empty %>  def destroy
+<%= empty %>    if @<%=singular_name%>.destroy
+<%= empty %>      flash[:notice] = '成功删除'
+<%= empty %>      redirect_to action: :index
+<%= empty %>    else
+<%= empty %>      flash[:error] = '删除失败'
+<%= empty %>      redirect_to action: :index
+<%= empty %>    end
+<%= empty %>  end
 
-  private
-    def <%=singular_name%>_params
-      params.require(:<%=singular_name%>).permit(<%=attributes.map{|a| ":#{a.name}"}.join(', ')%>)
-    end
+<%= empty %>  private
+<%= empty %>    def <%=singular_name%>_params
+<%= empty %>      params.require(:<%=singular_name%>).permit(<%=attributes.map{|a| ":#{a.name}"}.join(', ')%>)
+<%= empty %>    end
 
-    def set_<%=singular_name%>
-      @<%=singular_name %> = <%=singular_name.camelize %>.find(params[:id])
-    end
-end
+<%= empty %>    def set_<%=singular_name%>
+<%= empty %>      @<%=singular_name %> = <%=singular_name.camelize %>.find(params[:id])
+<%= empty %>    end
+<%= empty %>end
+<% class_path.each_with_index do |module_name, i|%><%= "  " * (class_path.count - i - 1)%>end
+<% end %>
