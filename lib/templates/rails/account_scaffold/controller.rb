d2969715af65e30c57c1132940c7801c9cd30ae5
houjinan@126.com <houjinan@126.com>
2016-09-21 16:51:06 +0800

    update account scaffold controller

--- lib/templates/rails/account_scaffold/controller.rb
+++ lib/templates/rails/account_scaffold/controller.rb
@@ -1,26 +1,22 @@
 <% empty = "  " * class_path.size;class_path.each_with_index do |module_name, i|%><%= "  " * i%>module <%=module_name.camelize%>
 <% end %>
 <%= empty %>class <%=singular_name.camelize%>Controller < Account::AccountController
-<%= empty %>  before_action :set_<%=singular_name%>, only: [:show, :edit, :update, :destroy]
+<%= empty %>  load_and_authorize_resource
 
 <%= empty %>  def index
-<%= empty %>    @<%=singular_name.pluralize %> = <%=singular_name.camelize %>.all.desc("created_at").paginate(:page => params[:page])
+<%= empty %>    @<%=singular_name.pluralize %> = @<%=singular_name.pluralize %>.paginate(:page => params[:page])
 <%= empty %>  end
 
 <%= empty %>  def show
-
 <%= empty %>  end
 
 <%= empty %>  def new
-<%= empty %>    @<%=singular_name %> = <%=singular_name.camelize %>.new()
 <%= empty %>  end
 
 <%= empty %>  def edit
-
 <%= empty %>  end
 
 <%= empty %>  def create
-<%= empty %>    @<%=singular_name%> = <%=singular_name.camelize %>.new(<%=singular_name%>_params)
 <%= empty %>    if @<%=singular_name%>.save
 <%= empty %>      flash[:notice] = '创建成功'
 <%= empty %>      redirect_to ({action: :show}.merge(id: @<%=singular_name%>.id))
@@ -51,10 +47,6 @@
 <%= empty %>  private
 <%= empty %>    def <%=singular_name%>_params
 <%= empty %>      params.require(:<%=singular_name%>).permit(<%=attributes.map{|a| ":#{a.name}"}.join(', ')%>)
-<%= empty %>    end
-
-<%= empty %>    def set_<%=singular_name%>
-<%= empty %>      @<%=singular_name %> = <%=singular_name.camelize %>.find(params[:id])
 <%= empty %>    end
 <%= empty %>end
 <% class_path.each_with_index do |module_name, i|%><%= "  " * (class_path.count - i - 1)%>end
